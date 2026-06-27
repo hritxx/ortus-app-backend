3dc3bf89ea4e70672f04c71d1583906d0f8afafa
hritxx <hriteekroy1869@gmail.com>
2026-06-27 18:23:09 +0530

    feat(bse): session token cache with TTL + serialized refresh

--- src/modules/bse/bse.module.ts
+++ src/modules/bse/bse.module.ts
@@ -3,10 +3,12 @@ import { ConfigModule } from "@nestjs/config";
 import { HttpModule } from "@nestjs/axios";
 import { PrismaModule } from "../../common/prisma/prisma.module";
 import { BseConfig } from "./bse.config";
+import { BseSoapClient } from "./bse-soap.client";
+import { BseSessionService } from "./bse-session.service";
 
 @Module({
   imports: [ConfigModule, HttpModule, PrismaModule],
-  providers: [BseConfig],
-  exports: [BseConfig],
+  providers: [BseConfig, BseSoapClient, BseSessionService],
+  exports: [BseConfig, BseSoapClient, BseSessionService],
 })
 export class BseModule {}
